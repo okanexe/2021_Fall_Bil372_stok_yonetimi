d1647af3bd21abdf6303c781b29c2d7476b5a2de
Okan Özşahin <okan.ozhn@gmail.com>
2021-11-24 19:02:15 +0300

    starting code for backend

---
+++ logistic/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import psycopg2
+
+app = Flask(__name__)
+
+# db yaratmak için
+# createdb -h localhost -p 5432 -U <username> <dbname>
+# postgresql bağlanmak için
+# psql -h localhost -p 5432 -d postgres
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/adress")
+def fetch():
+    conn = psycopg2.connect("dbname=guru99 user=okans password=")
+    cur = conn.cursor()
+
+    try:
+        cur.execute('select * from address')
+    except:
+        conn.rollback()
+    else:
+        conn.commit()
+
+    data = cur.fetchall()
+    return "<p>{}</p>".format(data[0])
+
+
+if __name__=='__main__':
+    app.run(debug=True)
