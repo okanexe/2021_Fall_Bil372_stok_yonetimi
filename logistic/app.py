c2e3a7b9e3a0cac0d6e4b0951510df57b92cb63a
Okan Özşahin <okan.ozhn@gmail.com>
2021-11-25 21:07:48 +0300

    db connect

--- logistic/app.py
+++ logistic/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import psycopg2
 
 app = Flask(__name__)
@@ -27,6 +27,27 @@ def fetch():
     data = cur.fetchall()
     return "<p>{}</p>".format(data[0])
 
+@app.route("/person/add", methods = ['POST', 'GET'])
+def person():
+    if request.method == 'POST':
+        result = request.form
+        conn = psycopg2.connect("dbname=guru99 user=okans password=")
+        cur = conn.cursor()
+
+        ssn = result.get("ssn")
+        personal_name = result.get("personal_name")
+        job_title = result.get("job_title")
+        email = result.get("email")
+        phone_number = result.get("phone_number")
+
+        insert_string = "INSERT INTO personal(ssn, personal_name, job_title, email, phone_number) " \
+                        "VALUES(%s, %s, %s, %s, %s);"
+        cur.execute(insert_string, (ssn, personal_name, job_title, email, phone_number))
+        print("inserted")
+        conn.commit()
+        return render_template("person.html", result=result)
+
+    return render_template("person.html")
 
 '''
 buradaki dashboad methodu personel ekleme ve personel yonetiminde kullanilacaktir 
@@ -38,6 +59,10 @@ def dashboard():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 if __name__=='__main__':
     app.run(debug=True)
